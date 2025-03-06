a8bebb26321a79d2228061f0763df9a11499e854
dan1delon <143418624+dan1delon@users.noreply.github.com>
2025-03-06 15:21:10 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -9,7 +10,7 @@ import cookieParser from 'cookie-parser';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = env('PORT', '3000');
 
 export const setupServer = () => {
   const app = express();
